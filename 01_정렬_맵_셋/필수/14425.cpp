c7cc6f9363650f8ee9360581ce29c85ac8cf1fc1
ynjii <kimyunji@ewhain.net>
2024-02-20 04:10:41 +0900

    [정렬, 맵, 셋] 2월 20일

--- 01_정렬_맵_셋/필수/14425.cpp
+++ 01_정렬_맵_셋/필수/14425.cpp
@@ -3,28 +3,29 @@
 
 using namespace std;
 
-
-int main(){
+int main() {
     int n, m;
     string str, checkstr;
     int count = 0;
-    //입력
+    
+    // 입력
     cin >> n >> m;
 
     set<string> s;
 
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++) {
         cin >> str;
         s.insert(str);
     }
 
-    for(int i=0;i<m;i++){
+    for (int i = 0; i < m; i++) {
         cin >> checkstr;
-        if(s.find(str)!=s.end()){
+        if (s.find(checkstr) != s.end()) {
             count++;
         }
     }
 
     cout << count << endl;
+
     return 0;
 }
